fa72bd183af16cd94716aec697e3fb51929115ac
Cornel Ghiban <cghiban@gmail.com>
2014-11-03 19:32:01 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.secret_key = 'ala bala'
+
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """convert a datetime to a different format."""
+    return value.strftime(format)
+
+app.jinja_env.filters['datetimefilter'] = datetimefilter
+
+
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      flash('You need to login first')
+      return redirect(url_for('login'))
+
+  return wrap
+
+
+@app.route('/')
+@login_required
+def home():
+  current_time=datetime.datetime.now()
+  return render_template('index.html', title="Home", current_time = current_time)
+
+
+@app.route('/welcome')
+def welcome():
+  return render_template('welcome.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if request.form['u'] != 'admin' or request.form['p'] != 'admin':
+      error = 'Invalid credentials. Try again'
+    else:
+      session['logged_in'] = True
+      flash("Just logged in")
+      return redirect(url_for('home'))
+  return render_template('login.html', error = error)
+
+@app.route('/logout')
+@login_required
+def logout():
+  session.pop('logged_in', None)
+  flash("Just logged out")
+  return redirect(url_for('welcome'))
+
+@app.route("/about")
+def about():
+    return render_template('index.html', title="About")
+
+@app.route("/contact")
+def contact():
+    return render_template('index.html', title="Contact Us")
+
+
+if __name__ == '__main__':
+  app.run(debug = True)
---
+++ test.py
@@ -0,0 +1,46 @@
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+  def test_index(self):
+    tester = app.test_client(self)
+    response = tester.get('/login', content_type='html/text')
+    self.assertEqual(response.status_code, 200)
+
+  def test_login_page_load(self):
+    tester = app.test_client(self)
+    response = tester.get('/login', content_type='html/text')
+    self.assertTrue(b'Please login' in response.data)
+
+  def test_login_works_on_invalid_credentials(self):
+    tester = app.test_client(self)
+    response = tester.post('/login', data=dict(u='x', p='y'), follow_redirects = True)
+    self.assertTrue(b'Error:' in response.data)
+
+  def test_login_works_on_post(self):
+    tester = app.test_client(self)
+    response = tester.post(
+        '/login', 
+        data=dict(u='admin', p='admin'), 
+        follow_redirects = True
+    )
+    self.assertEqual(response.status_code, 200)
+
+  def test_logout(self):
+    tester = app.test_client(self)
+    tester.post(
+        '/login', 
+        data=dict(u='admin', p='admin'), 
+        follow_redirects = True
+    )
+    response = tester.get('/logout', follow_redirects=True)
+    self.assertIn(b'Just logged out', response.data)
+
+  def test_main_route_requires_login(self):
+    tester = app.test_client(self)
+    response = tester.get('/', follow_redirects=True)
+    self.assertTrue('Please login' in response.data)
+
+
+if __name__ == '__main__':
+  unittest.main()
